a95215a157f7a6af9ec75a159ba10dd676ed696f
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-20 05:11:52 -0700

    menambahkan class mahasiswa

--- Komposisi/jantung.h
+++ Komposisi/jantung.h
@@ -5,3 +5,13 @@ using namespace std;
 #define JANTUNG_H
 
 using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+}; 
