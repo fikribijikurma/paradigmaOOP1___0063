92f0c82eca5dcaa1faca4218068c212074658f16
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-20 05:23:21 -0700

    membuat header anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -1,15 +1,2 @@
 #ifndef ANAK.H
 #define ANAK.H
-
-class Anak {
-public:
-	string nama;
-	anak(string pNama) :nama(pNama) {
-		cout << "Anak \"" << nama << "\" ada\n";
-	}
-
-	~anak() {
-		cout << "Anak \"" << nama << "\" tidak ada\n";
-	}
-};
-#endif
